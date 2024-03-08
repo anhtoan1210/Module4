63bbd40df217babfeea59da28380f16292b49b78
ADMIN <lequangtoan12102000@gmail.com>
2024-03-08 15:49:36 +0700

    up

---
+++ ss3_bt/src/main/java/org/example/ss3_bt/HelloApplication.java
@@ -0,0 +1,9 @@
+package org.example.ss3_bt;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
